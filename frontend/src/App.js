7eb1da4aa9b5212658fa684eb8e3f4bc9aaf043f
shalini.e.verma <shalini.e.verma@accenture.com>
2022-04-25 10:52:42 +0530

    Adding my frontend home and product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <>
-      <h1>Hello react</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
